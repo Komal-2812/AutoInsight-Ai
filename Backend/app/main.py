1493f2f52dbafb113bfcb7f734f56a24a9c4321a
Komal-2812 <sutharkomal234@gmail.com>
2026-04-12 20:36:27 +0530

    fix: added env types and favicon

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -16,8 +16,7 @@ app = FastAPI(
 # ── CORS Configuration ────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"],
+    allow_origins=["*"],  # later restrict
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
